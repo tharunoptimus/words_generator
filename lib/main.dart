70ba5556f6085b0d60bf0acccc0e953001308fe7
Tharunraj Optimus <tharunoptimus@outlook.com>
2021-08-06 13:54:09 +0530

    Default ListView in _buildList

---
+++ lib/main.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final wordPair = WordPair.random();
+    return MaterialApp(
+        theme: ThemeData(primaryColor: Colors.purple[900]),
+        home: RandomWords());
+  }
+}
+
+class RandomWords extends StatefulWidget {
+  @override
+  RandomWordsState createState() => RandomWordsState();
+}
+
+class RandomWordsState extends State<RandomWords> {
+  Widget _buildList() {
+    return (ListView(padding: const EdgeInsets.all(8), children: <Widget>[
+      Container(
+          height: 50,
+          color: Colors.amber[600],
+          child: const Center(
+            child: Text("Entry A"),
+          )),
+      Container(
+          height: 50,
+          color: Colors.amber[700],
+          child: const Center(
+            child: Text("Entry B"),
+          )),
+      Container(
+          height: 50,
+          color: Colors.amber[800],
+          child: const Center(
+            child: Text("Entry C"),
+          ))
+    ]));
+  }
+
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Word Pair Generator'),
+        ),
+        body: _buildList());
+  }
+}
